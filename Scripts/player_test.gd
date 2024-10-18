cf6b0695373de3041f81fa7ab61e7fab154e9599
datrekap <dkapoor3008@gmail.com>
2024-10-17 21:49:26 -0400

    Changes made by DK - OCT 17
    discussion with Sargylana (learning GODOT)

--- Scripts/player_test.gd
+++ Scripts/player_test.gd
@@ -9,7 +9,7 @@ func get_input():
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_ESCAPE:
-			get_tree().quit()
+			get_tree().change_scene_to_file("res://Scenes/Main menu test.tscn")
 
 func _physics_process(_delta):
 	get_input()
