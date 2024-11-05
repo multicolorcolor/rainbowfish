78f4f22ef85f094fed800f2de07143a030640fc7
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2024-11-05 15:01:11 -0500

    fish_collect_fixed
    Spare fishes deleted. UI elements and camera repositioned. Player physics paused during the flashbacks

--- Scenes/flashbacksManager.gd
+++ Scenes/flashbacksManager.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var flashback2 = $Flashback2
 @onready var flashback3 = $Flashback3
 @onready var flashback4 = $Flashback4
+signal FlashbackTime
+signal FlashbackOver
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,10 +16,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept") and SignalBus.fish == 1:
 		flashback1.visible = false
+		emit_signal("FlashbackOver")
 	if Input.is_action_pressed("ui_accept") and SignalBus.fish == 2:
 		flashback2.visible = false
+		emit_signal("FlashbackOver")
 	if Input.is_action_pressed("ui_accept") and SignalBus.fish == 3:
 		flashback3.visible = false
+		emit_signal("FlashbackOver")
 	if Input.is_action_pressed("ui_accept") and SignalBus.fish == 4:
 		flashback4.visible = false
 
@@ -25,14 +30,17 @@ func _process(delta: float) -> void:
 
 func _on_fish_1_fishcollected() -> void:
 	flashback1.visible = true
+	emit_signal("FlashbackTime")
 
 
 func _on_fish_2_fishcollected() -> void:
 	flashback2.visible = true
+	emit_signal("FlashbackTime")
 
 
 func _on_fish_3_fishcollected() -> void:
 	flashback3.visible = true
+	emit_signal("FlashbackTime")
 
 
 func _on_fish_4_fishcollected() -> void:
