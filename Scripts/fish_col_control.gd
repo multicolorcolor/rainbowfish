051056b939a20f5020ab2ae5c0b046b89e7dbf23
datrekap <dkapoor3008@gmail.com>
2024-11-15 14:38:44 -0500

    Fish animation added to the Fish sprite

--- Scripts/fish_col_control.gd
+++ Scripts/fish_col_control.gd
@@ -3,8 +3,8 @@ signal Fishcollected()
 @onready var animationFish = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	animationFish.play("Swim") # Replace with function body.
+func _ready():
+	$AnimationPlayer.play("Swim") # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
