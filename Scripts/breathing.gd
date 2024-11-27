9ec75c5dfe4d5647e1df45064381d376ff21b1f7
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2024-11-27 16:22:43 -0500

    breathing

--- Scripts/breathing.gd
+++ Scripts/breathing.gd
@@ -1,15 +1,20 @@
 extends ProgressBar
 @export var max_health: float
 var health
+var tweenBreathe = create_tween()
+var tweenBreatheRefill = create_tween()
+@onready var anim = get_node("%TransitionAnimations")
 
 func _ready() -> void:
 	health = max_health
 	value = 100
+	
 
 func _process(delta: float):
-	if Input.is_action_just_pressed("ui_up"):
-		print("taking damage")
-		take_damage()
+	if value == 0:
+		anim.play("DeathScreen")
+		await get_tree().create_timer(2).timeout
+		get_tree().change_scene_to_file("res://Scenes/Main menu test.tscn")
 	
 func take_damage():
 	var tweenBreathe = create_tween()
@@ -17,10 +22,14 @@ func take_damage():
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	var tweenBreathe = create_tween()
-	tweenBreathe.tween_property(self, "value", 0, 2)
+	if tweenBreathe:
+		tweenBreathe.kill() 
+	tweenBreathe = create_tween()
+	tweenBreathe.tween_property(self, "value", 0, 50)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	var tweenBreatheRefill = create_tween()
+	if tweenBreathe:
+		tweenBreathe.kill() 
+	tweenBreatheRefill = create_tween()
 	tweenBreatheRefill.tween_property(self, "value", 100, 2)
